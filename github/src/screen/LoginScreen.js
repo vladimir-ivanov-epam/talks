623d4aa474d90f2b7d380fd09c6384e313199455
Vladimir Ivanov <vladimir.vs.ivanov@gmail.com>
2017-09-05 15:48:13 +0300

    Login action

--- github/src/screen/LoginScreen.js
+++ github/src/screen/LoginScreen.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {View} from "react-native";
+import {Text, View} from "react-native";
 
 import Logo from "./Logo";
 import LoginInputs from "./LoginInputs";
 import {Button} from "react-native-elements";
+import {loginAsync} from "../actions/LoginActions";
 
 class LoginScreen extends Component {
+
+    state = { error: null }
 
     changeLogin = (login) => {
         this.setState({ 'login': login });
@@ -15,12 +18,19 @@ class LoginScreen extends Component {
         this.setState({ 'password': password });
     };
 
-    doLogin = () => {
-
-    }
+    doLogin = async () => {
+        this.setState({ error: null, loggedIn: false });
+        const { login, password } = this.state;
+        try {
+            let result = await loginAsync(login, password);
+            this.setState({ loggedIn: result.user !== undefined, error: result.error });
+        } catch (error) {
+            this.setState({ error: error });
+        }
+    };
 
     render() {
-        const {container} = styles;
+        const {container, successMessage, errorMessage} = styles;
 
         console.log("[LoginScreen] state = " + JSON.stringify(this.state));
 
@@ -37,6 +47,8 @@ class LoginScreen extends Component {
                     backgroundColor="#00F"
                     onPress={this.doLogin}
                 />
+                {this.state.loggedIn && <Text style={successMessage}>Logged in!</Text>}
+                {this.state.error && <Text style={errorMessage}>{this.state.error}</Text>}
             </View>
         )
     }
@@ -49,6 +61,20 @@ const styles = {
         backgroundColor: 'white',
         flexGrow: 1,
         flex: 1
+    },
+    successMessage: {
+        margin: 16,
+        justifyContent: 'center',
+        color: '#00F',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    errorMessage: {
+        margin: 16,
+        justifyContent: 'center',
+        color: '#F00',
+        fontSize: 18,
+        textAlign: 'center'
     }
 };
 
